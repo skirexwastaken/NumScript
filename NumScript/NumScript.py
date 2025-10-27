82f44dd2e3e3e142d0e58813afd9a01455996409
Daniel <hora.daniel@protonmail.com>
2025-10-28 00:33:37 +0100

    Delete NumScript directory

--- NumScript/NumScript.py
+++
@@ -1,9 +0,0 @@
-# --- NumScript ---
-
-if __name__ == "__main__":  
-    # --- Importing NumScript Class from Source ---
-    from Source.builder import NumScript
-
-    # --- Launching the NumScript shell ---                
-    shell = NumScript()
-    shell.cli()
