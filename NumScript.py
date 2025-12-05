a39e6f0ef91abebe57c3bfbd995706aba3237e73
Daniel <hora.daniel@protonmail.com>
2025-12-05 22:39:06 +0100

    Add files via upload

--- NumScript.py
+++ NumScript.py
@@ -1,10 +1,8 @@
 # --- NumScript ---
 
-if __name__ == "__main__":  
-    
-    # --- Importing NumScript Virtual Machine from Source ---
-    from Source.builder import NumScriptVirtualMachine
+# --- Importing NumScript Virtual Machine from Source ---
+from Source.builder import NumScriptVirtualMachine
 
-    # --- Launching the NumScript shell ---                
-    engine = NumScriptVirtualMachine()
-    engine.cli()
+# --- Launching the NumScript shell ---                
+engine = NumScriptVirtualMachine()
+engine.cli()
