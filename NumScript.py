8ce63c9ab355ba42b75ccb21bbe9e457be9d0227
Daniel <hora.daniel@protonmail.com>
2025-12-12 19:02:20 +0100

    Delete NumScript.py

--- NumScript.py
+++
@@ -1,8 +0,0 @@
-# --- NumScript ---
-
-# --- Importing NumScript Virtual Machine from Source ---
-from Source.builder import NumScriptVirtualMachine
-
-# --- Launching the NumScript shell ---                
-engine = NumScriptVirtualMachine()
-engine.cli()
